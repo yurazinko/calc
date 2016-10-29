0d3ff5868b995b9b789d46286beb623aaa8dd08b
Yurii Zinko <yurazinko@outlook.com>
2016-10-29 16:28:14 +0300

    tokenize

--- calc.rb
+++ calc.rb
@@ -1,5 +1,13 @@
 def calculate(str)
-	array = str.split(%r{\s*})
+    arr = str.split('')
+    symbols = %w{ + - / * ( ) }
+    arr.collect! do |c| 
+        if symbols.include?(c)
+            c.to_sym
+        else
+            c.to_i
+        end
+    end
 end
 
 calculate('(5+10)*2')
